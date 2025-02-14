b481b9a39d2c2df597884efef5762610a47171cd
Fabian <varga.aditya@gmail.com>
2025-02-14 23:10:04 +0700

    finish4

--- app/Filament/Resources/BoardingHouseResource.php
+++ app/Filament/Resources/BoardingHouseResource.php
@@ -110,7 +110,11 @@ class BoardingHouseResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('city.name'),
+                Tables\Columns\TextColumn::make('category.name'),
+                Tables\Columns\TextColumn::make('price'),
+                Tables\Columsns\ImageColumn::make('thumbnail'),
             ])
             ->filters([
                 //
